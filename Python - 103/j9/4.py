3e51ab10a260eeefa0ca76e4d967dfb20a146fb7
Samiar Akbarivand <yarsam071@gmail.com>
2026-09-16 00:29:52 +0330

    Add files via upload

---
+++ Python - 103/j9/4.py
@@ -0,0 +1,7 @@
+l1 = [1 , 2 , 3]
+l2 = [40 , 5 , 16]
+
+l1.clear()
+l2.sort(reverse=False)
+print(l2)
+print(l1)
---
+++ Python - 103/j9/5.py
@@ -0,0 +1,4 @@
+a = [1 , 2 , 3 , 4 , "HI" , "True" , "None"]
+
+for i in a:
+    print(i)
